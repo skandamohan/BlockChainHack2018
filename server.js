0fd7493e74b36b9976effc13f1494f108c307344
skandamohan <skandaman@gmail.com>
2018-03-31 15:28:18 -0400

    first

---
+++ server.js
@@ -0,0 +1,11 @@
+//server.js
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+const port = 8000
+app.listen(port,() => {
+   console.log ('We are live on '+port);
+});
+
